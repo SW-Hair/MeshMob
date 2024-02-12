50e3e6df498fa668df8a0141116efa0ebf9bec9d
SW-Hair <150551862+SW-Hair@users.noreply.github.com>
2024-02-12 02:08:34 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -10,28 +10,30 @@ const controller = new ScrollMagic.Controller();
 
 //Scenes
 let scene = new ScrollMagic.Scene({
- duration: 13000,
- triggerElement: intro,
- triggerHook: 0
+ duration: 13000,
+ triggerElement: intro,
+ triggerHook: 0
 })
- .setPin(intro)
- .addTo(controller);
+ .setPin(intro)
+ .addTo(controller);
 
 //Text Animation
 const textAnim = TweenMax.fromTo(text, 0, { opacity: 1 }, { opacity: 0 });
 
 let scene2 = new ScrollMagic.Scene({
-    duration: 0,
-    triggerElement: 0,
-    triggerHook: 0
+    duration: 0,
+    triggerElement: 0,
+    triggerHook: 0
 })
 .setTween(textAnim)
 .addTo(controller);
 
- //Video Animation
- let accelamount = 1;
- let scrollpos = 0;
- let delay = 0;
+//Video Animation (Replace this section with the following)
+let accelamount = 1; // Keep your original acceleration
+let scrollpos = 0;
+let delay = 0;
+
+const isIOS = /iPad|iPhone|iPod/.test(navigator.platform); 
 
 scene.on("update", e => {
     scrollpos = e.scrollPos / 1000;
@@ -40,7 +42,13 @@ scene.on("update", e => {
 function updateVideoTime() {
     delay += (scrollpos - delay) * accelamount;
     delay = parseFloat(delay.toFixed(2));
-    video.currentTime = delay;
-    requestAnimationFrame(updateVideoTime);
+
+    if (isIOS && delay > 0.1) { // iOS optimization tweak
+        video.currentTime = delay;
+    } else {
+        video.currentTime = delay;
+    }
+
+    requestAnimationFrame(updateVideoTime); 
 }
-updateVideoTime();
+updateVideoTime(); 
